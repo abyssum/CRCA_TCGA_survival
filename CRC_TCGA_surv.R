0a1e6ebe276a830910662c472358860801d9c91e
abyssum <fotakisg@gmail.com>
2024-08-23 15:19:54 +0200

    load in data

--- CRC_TCGA_surv.R
+++ CRC_TCGA_surv.R
@@ -15,3 +15,17 @@ filter_and_trim <- function(data, column_name, lFC_threshold, n_rows) {
   return(trimmed_data)
 }
 
+# Load in the data
+clinical <- read.table("/home/fotakis/myScratch/PHD_Project/TCGA_survival/clinicalCRC.txt")
+
+expression.clean <- read.csv("/home/fotakis/myScratch/PHD_Project/TCGA_survival/expression_COADREAD_clean.tsv",
+                             header = T,
+                             sep = ",")
+
+mappings <- read.csv("/home/fotakis/myScratch/PHD_Project/TCGA_survival/GDCidMapping_20190712_full.tsv",
+                     header = T,
+                     sep = "\t")
+
+crca.de.genes <- read.csv2("/home/fotakis/myScratch/CRCA_survival/immune_infiltration-DESeq2_result.tsv", 
+                           header = T, 
+                           sep = "\t")
